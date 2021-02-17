723b568a21a36182793449d7a53f684f9c7a0cf5
Matheus Cordeiro <matheus.cordeiro@muralis.com.br>
2021-02-16 23:15:29 -0300

    DONE: Mobile ver.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,14 +6,14 @@ const RESOURCES = {
   "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"assets/NOTICES": "64cdc3e367f28380775c7c89822f1e66",
+"assets/NOTICES": "cf0cf34139027e0f359d3bcc2bd83a38",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "b14fcf3ee94e3ace300b192e9e7c8c5d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "cef8e5c485995076685ebb9f565a37ef",
-"/": "cef8e5c485995076685ebb9f565a37ef",
-"main.dart.js": "ca40dd338e2d1b59dc315f4b10b5cbcc",
+"index.html": "6b860550de1ea7b0a596fef1ce512b62",
+"/": "6b860550de1ea7b0a596fef1ce512b62",
+"main.dart.js": "11bf37efea082546037738301527c7f3",
 "manifest.json": "fc4c260e20cd1eff4d98537443a92ceb",
 "version.json": "38e742aa02019516f252925078fa50f4"
 };
